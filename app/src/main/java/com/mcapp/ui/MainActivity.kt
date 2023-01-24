019d8af489ce587386704c294c128b8f919c30fa
Essi Passoja <essi.passoja@gmail.com>
2023-01-24 20:21:30 +0200

    Repository design changes

--- app/src/main/java/com/mcapp/ui/MainActivity.kt
+++ app/src/main/java/com/mcapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.mcapp
+package com.mcapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mcapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/mcapp/util/NotificationWorker.kt
@@ -0,0 +1,4 @@
+package com.mcapp.util
+
+class NotificationWorker {
+}
