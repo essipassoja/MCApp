d12334e0afb963d3c5bda35aec2bb5019003405a
Essi Passoja <essi.passoja@gmail.com>
2023-02-20 19:51:58 +0200

    Call reminderRepository not impl

--- app/src/main/java/com/mcapp/ui/reminder/ReminderViewModel.kt
+++ app/src/main/java/com/mcapp/ui/reminder/ReminderViewModel.kt
@@ -3,29 +3,29 @@ package com.mcapp.ui.reminder
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.mcapp.data.entity.Reminder
-import com.mcapp.data.repository.ReminderRepositoryImpl
+import com.mcapp.data.repository.ReminderRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 
-class ReminderViewModel(private val reminderRepositoryImpl: ReminderRepositoryImpl): ViewModel() {
+class ReminderViewModel(private val reminderRepository: ReminderRepository): ViewModel() {
     private val _viewReminders = MutableStateFlow<ReminderViewState>(ReminderViewState.Loading)
     val reminders: StateFlow<ReminderViewState> = _viewReminders
 
     fun insertOrUpdateReminder(reminder: Reminder) {
-        viewModelScope.launch { reminderRepositoryImpl.insertOrUpdate(reminder) }
+        viewModelScope.launch { reminderRepository.insertOrUpdate(reminder) }
     }
 
     // TODO make a notification for new reminder successfully made
 
     fun deleteReminder(reminder: Reminder) {
-        viewModelScope.launch { reminderRepositoryImpl.delete(reminder) }
+        viewModelScope.launch { reminderRepository.delete(reminder) }
     }
 
     fun getListOfAllReminders(creatorId: Long) {
         viewModelScope.launch {
-            reminderRepositoryImpl.getAllReminders(creatorId).map {
+            reminderRepository.getAllReminders(creatorId).map {
                 _viewReminders.value = ReminderViewState.Success(it)
             } }
     }
