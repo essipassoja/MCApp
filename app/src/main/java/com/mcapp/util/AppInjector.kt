6c2bf68cc4e3f542d5286c3d574f4bb9425ba15f
Essi Passoja <essi.passoja@gmail.com>
2023-02-20 19:52:34 +0200

    Added ReminderRepository initialization to inject

--- app/src/main/java/com/mcapp/util/AppInjector.kt
+++ app/src/main/java/com/mcapp/util/AppInjector.kt
@@ -4,14 +4,15 @@ import androidx.room.Room
 import com.mcapp.data.MCDatabase
 import com.mcapp.data.datasource.ReminderDataSource
 import com.mcapp.data.datasource.ReminderDataSourceImpl
+import com.mcapp.data.repository.ReminderRepository
 import com.mcapp.data.repository.ReminderRepositoryImpl
 import com.mcapp.ui.reminder.ReminderViewModel
 import org.koin.dsl.module
 
 val reminderModule = module {
-    single { ReminderViewModel(get()) }
     single<ReminderDataSource> { ReminderDataSourceImpl(get()) }
-    single { ReminderRepositoryImpl(get()) }
+    single<ReminderRepository> { ReminderRepositoryImpl(get()) }
+    single { ReminderViewModel(get()) }
 }
 
 val appStatusModule = module {
